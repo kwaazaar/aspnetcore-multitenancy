a577f8e810087f9af2f5a380b93820425d609910
robertka <robertka@infosupport.com>
2017-10-24 14:43:02 +0200

    Add project files.

---
+++ ConfigConsul/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConfigConsul
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
